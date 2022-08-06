8ac853c4c3e0ca764f1debc2414e43a6b8c7688a
Joseph Arvanitis <58145194+josarv@users.noreply.github.com>
2022-08-07 01:05:57 +0300

    one liner for 4

--- 4 - largest palindrome product/solution.jl
+++ 4 - largest palindrome product/solution.jl
@@ -1,8 +1,3 @@
 #!/usr/bin/env julia
 
-function is_palindrome(n)
-    s = string(n)
-    return s == reverse(s)
-end
-
-println(maximum(filter(is_palindrome, [i * j for i in 999:-1:100 for j in 999:-1:100])))
+println(maximum(filter(function(n) s = string(n); return s == reverse(s) end, [i * j for i in 999:-1:100 for j in 999:-1:100])))
