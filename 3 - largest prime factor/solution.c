ff5156378223cc75e81db3bc90a360a895a1f326
Joseph Arvanitis <58145194+josarv@users.noreply.github.com>
2022-08-07 04:06:37 +0300

    minor tweaks

--- 3 - largest prime factor/solution.c
+++ 3 - largest prime factor/solution.c
@@ -1,7 +1,7 @@
 #include <stdbool.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 #define N 600851475143
 
--- 4 - largest palindrome product/solution.py
+++ 4 - largest palindrome product/solution.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 
-def is_palindrome(s):
-    return s == s[::-1]
-
 
 def main():
     maximum = 1
     for i in range(1, 999):
         for j in range(1, 999):
             product = i * j
-            if product > maximum and is_palindrome(str(product)):
-                maximum = product
+            if product > maximum:
+                s = str(product)
+                if s == s[::-1]:
+                    maximum = product
     print(maximum)
 
 
