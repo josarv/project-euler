75e7c6a0131dd08af396eedc359f1f1d1b8405d8
Joseph Arvanitis <58145194+josarv@users.noreply.github.com>
2022-08-07 14:56:09 +0300

    add/tweak 4

--- 4 - largest palindrome product/solution.jl
+++ 4 - largest palindrome product/solution.jl
@@ -1,3 +1,3 @@
 #!/usr/bin/env julia
 
-println(maximum(filter(function(n) s = string(n); return s == reverse(s) end, [i * j for i in 999:-1:100 for j in 999:-1:100])))
+println(maximum(i * j for i in 999:-1:100 for j in 999:-1:100 if string(i * j) == reverse(string(i * j))))
--- 4 - largest palindrome product/solution.py
+++ 4 - largest palindrome product/solution.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python3
 
+from itertools import product
+
 
 def main():
-    maximum = 1
-    for i in range(100, 999):
-        for j in range(100, 999):
-            product = i * j
-            if product > maximum:
-                s = str(product)
-                if s == s[::-1]:
-                    maximum = product
-    print(maximum)
+    print(max(filter(lambda x: str(x) == str(x)[::-1], map(lambda x: x[0] * x[1], product(range(100, 1000), repeat=2)))))
 
 
 if __name__ == '__main__':
