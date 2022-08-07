2edc382eed29b4732225c6ce51b2f80717365b21
Joseph Arvanitis <58145194+josarv@users.noreply.github.com>
2022-08-07 04:50:44 +0300

    make python for 4 faster

--- 4 - largest palindrome product/solution.py
+++ 4 - largest palindrome product/solution.py
@@ -3,8 +3,8 @@
 
 def main():
     maximum = 1
-    for i in range(1, 999):
-        for j in range(1, 999):
+    for i in range(100, 999):
+        for j in range(100, 999):
             product = i * j
             if product > maximum:
                 s = str(product)
